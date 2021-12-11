e11a68c2c8a5d40a30f179055d83122f136ba1e0
juniorpeves <juniorpeves@gmail.com>
2021-12-11 17:05:46 -0500

    models_tweet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Python
 from uuid import UUID
 from datetime import date
+from datetime import datetime
 from typing import Optional
 
 # Pydantic
@@ -40,7 +41,15 @@ class User(UserBase):
     birth_date: Optional[date] = Field(default=None)
     
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+    )
+    create_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 
 
