73196076a83504ba7dc6c9fbd0c444eeeb42efcd
juniorpeves <juniorpeves@gmail.com>
2021-12-11 16:59:51 -0500

    models_user

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
+# Python
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel # Permite crear los modelos
+from pydantic import EmailStr # Valida emails
+from pydantic import Field # Validar los atributos de un modelo
+
+# FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
+
+#Models
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64        
+    )
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+    
+class Tweet(BaseModel):
+    pass
+
+
 
 @app.get(path="/")
 def home():
